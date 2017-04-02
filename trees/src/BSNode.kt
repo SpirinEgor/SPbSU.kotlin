95f80105852e681995093cac7175a8dd52b3af86
voudy <spirin.egor@mail.ru>
2017-04-02 19:22:48 +0300

    delete

--- trees/src/BSNode.kt
+++
@@ -1,7 +0,0 @@
-public class BSNode<K : Comparable<K>, V>(override var key: K, override var value: V) : Node<K, V>{
-
-    var left: BSNode<K, V>? = null
-    var right: BSNode<K, V>? = null
-    override var size: Int = 0
-
-}
