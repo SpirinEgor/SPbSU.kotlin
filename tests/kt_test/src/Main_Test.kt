fdbf58ddd7bf4daddb74d6a0a619c81134034a3d
voudy <spirin.egor@mail.ru>
2017-04-02 19:25:44 +0300

    true delete

--- tests/kt_test/src/Main_Test.kt
+++
@@ -1,9 +0,0 @@
-import junit.framework.TestCase
-
-class Main_Test : TestCase() {
-
-    fun testGetMax() {
-        assertEquals(3, getMax(1, 3))
-    }
-
-}
--- tests/kt_test/src/main.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>){
-}
-
-fun getMax(a: Int , b: Int): Int{
-    if (a > b) return a
-    else return b
-}
